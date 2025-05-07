632680c7d29bf4e205bb24d3939aee6a879857a2
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-05-07 16:33:00 +0100

    Sketch ex02 solution

---
+++ ex02/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	string;
+	std::string	*stringPTR;
+	
+	string = "HI THIS BRAIN";
+	stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "Memory address string: " << stringPTR << std::endl;
+	std::cout << "Memory address stringPTR: " << &stringPTR << std::endl;
+	std::cout << "Memory address stringRE: " << &stringREF << std::endl;
+
+	std::cout << "\n";
+
+	std::cout << "Value string variable: " << string << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+	return (0);
+}
