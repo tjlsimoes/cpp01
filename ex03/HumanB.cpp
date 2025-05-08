2da70fb452e2828ce0288bf1a3c9808dab63b3fb
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-05-08 15:32:00 +0100

    Set c++98 flag

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: tjorge-l < tjorge-l@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 16:56:26 by tjorge-l          #+#    #+#             */
-/*   Updated: 2025/05/07 17:45:10 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2025/05/08 15:31:00 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ HumanB::~HumanB(void) {}
 
 void	HumanB::attack(void)
 {
-	if (this->weapon == nullptr)
+	if (!(this->weapon))
 		return ;
 	std::cout
 		<< this->name
